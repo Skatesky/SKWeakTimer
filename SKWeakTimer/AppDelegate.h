2638abdeb9b3f6ebf0cbd2c006a35786e4629b70
zhanghuabing <zhanghuabing@douyu.tv>
2018-10-16 18:17:13 +0800

    init

---
+++ SKWeakTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKWeakTimer
+//
+//  Created by zhanghuabing on 2018/10/16.
+//  Copyright © 2018年 zhb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
